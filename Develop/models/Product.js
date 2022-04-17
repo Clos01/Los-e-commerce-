a407903141c18f069c82dcf08c9dd89a995eb75d
Carlos Rivas <Cjrivas00@gmail.com>
2022-04-16 23:34:02 -0400

    added the id for category so it can be categorised and validted for stock and what kind of product it is

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -21,10 +22,33 @@ Product.init(
         product_name: {
             type: DataTypes.STRING,
             allowNull: false
-
           },
-
-
+          price: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNul: false,
+            validate:{
+        isDecimal: true
+            }
+          },
+          //Define stock colum 
+          Stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate:{
+              isNumeric: true
+            }
+          },
+          // define category_id column
+          category_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references:{
+              model: "category",
+              key: "id",
+            }
+          }
+        },  
 
   {
     sequelize,
